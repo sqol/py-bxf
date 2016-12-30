af16f52164cad5b1795294f3d977bc9304e4c94d
Oli <oli.tipper+github@gmail.com>
2016-12-30 13:56:24 -0700

    Basic message processing
    Pulling messageId, Origin, messageType etc.

--- py-bxf.py
+++ py-bxf.py
@@ -8,12 +8,39 @@
 ##  sqol 30-dec-2016
 ################################################################################
 
+## todo
+# - watch folder, configurable read time, check for file completion
+# - socket read/write
+# - entries into database
+# - ack/nack
+
 import sys
 import logging
 import time
 import pprint
 
+#from bs4 import BeautifulSoup
+from urllib import urlopen
+import sqlite3
+import xml.etree.ElementTree as ET
+
+## variable holding pen
+HandlerName = "pybxf"
+BXFWatchDir = "C:/temp/bxf/input/"
+BXFOutputDir = "C:/temp/bxf/output/"
+BXFErrorDir = "C:/temp/bxf/error/"
+BXFFileExt = ".bxf"
+
+
 ## logging setup
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.info("py-bxf started, " + time.strftime("%c"))
+
+tree = ET.parse(BXFWatchDir + 'TransferRequestComplete' + BXFFileExt)
+root = tree.getroot()
+print "BxfMessageId: %s" % root.attrib['id'].strip("urn:uuid:")
+print "Origin: %s" % root.attrib['origin']
+print "Destination: %s" % root.attrib['destination']
+print "MessageType1: %s" % root.attrib['messageType']
+print "MessageType2: %s" % root[0][0].tag.split('}', 1)[-1] # remove the schema location from front
